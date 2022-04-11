5dfcb59c16a90b55c6f06ea56dd4c88aae1c4f9b
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-04-11 21:37:29 +0800

    SSS: updated User entity to override hashcode, equals and toString() methods

--- src/main/java/com/anima/socialsecuritysystem/entities/model/User.java
+++ src/main/java/com/anima/socialsecuritysystem/entities/model/User.java
@@ -1,7 +1,7 @@
 package com.anima.socialsecuritysystem.entities.model;
 
 import com.anima.socialsecuritysystem.entities.BaseModel;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,8 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Objects;
 
+@Data
 @Entity
 @Table(name ="SSS_USER")
 public class User extends BaseModel<String> implements Serializable {
@@ -85,48 +86,30 @@ public class User extends BaseModel<String> implements Serializable {
         this.dateOfBirth = dateOfBirth;
     }
 
-    /**
-     * Returns a hash code value for the object. This method is
-     * supported for the benefit of hash tables such as those provided by
-     * {@link HashMap}.
-     * <p>
-     * The general contract of {@code hashCode} is:
-     * <ul>
-     * <li>Whenever it is invoked on the same object more than once during
-     *     an execution of a Java application, the {@code hashCode} method
-     *     must consistently return the same integer, provided no information
-     *     used in {@code equals} comparisons on the object is modified.
-     *     This integer need not remain consistent from one execution of an
-     *     application to another execution of the same application.
-     * <li>If two objects are equal according to the {@code equals(Object)}
-     *     method, then calling the {@code hashCode} method on each of
-     *     the two objects must produce the same integer result.
-     * <li>It is <em>not</em> required that if two objects are unequal
-     *     according to the {@link Object#equals(Object)}
-     *     method, then calling the {@code hashCode} method on each of the
-     *     two objects must produce distinct integer results.  However, the
-     *     programmer should be aware that producing distinct integer results
-     *     for unequal objects may improve the performance of hash tables.
-     * </ul>
-     *
-     * @return a hash code value for this object.
-     * @implSpec As far as is reasonably practical, the {@code hashCode} method defined
-     * by class {@code Object} returns distinct integers for distinct objects.
-     * @see Object#equals(Object)
-     * @see System#identityHashCode
-     */
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return Objects.hash(id, firstname, middlename, lastname, suffix, dateOfBirth, contactNumber, email);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) && Objects.equals(firstname, user.firstname) && Objects.equals(middlename, user.middlename) && lastname.equals(user.lastname) && Objects.equals(contactNumber, user.contactNumber);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "User{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", middlename='" + middlename + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", suffix='" + suffix + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", contactNumber='" + contactNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
